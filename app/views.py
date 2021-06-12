04a90f9e7c3d93865fb20db6a7ca7b66a18a22ae
Shiva Thapa <sam.aquarkorius8@gmail.com>
2021-06-13 00:22:19 +0545

    added background color, thumbail support, aligned items

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ def app(request):
 		url = request.POST['url']
 		yt = YouTube(url)
 		stream = yt.streams.get_lowest_resolution()
+		thumbnail = yt.thumbnail_url
 		stream.download()
-		return render(request, 'app/app.html')
+		return render(request, 'app/app.html', context = {'thumbnail_img': thumbnail})
 	return render(request, 'app/app.html')
